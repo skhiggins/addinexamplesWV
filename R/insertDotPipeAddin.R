969836eccfff7f145fb0d7fbd3528721773d221a
John Mount <jmount@win-vector.com>
2017-11-17 18:39:35 -0800

    Add wrapr dotpipe addin

---
+++ R/insertDotPipeAddin.R
@@ -0,0 +1,8 @@
+#' Insert \%.>\%.
+#'
+#' Call this function as an addin to insert \code{\link[wrapr]{\%.>\%}} at the cursor position.
+#'
+#' @export
+insertDotPipeAddin <- function() {
+  rstudioapi::insertText(" %.>% ")
+}

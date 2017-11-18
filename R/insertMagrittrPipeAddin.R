83a62969f4bf742e5ea338e506b4b6b73d97a5ef
John Mount <jmount@win-vector.com>
2017-11-18 09:48:36 -0800

    add Magrittr pipe

---
+++ R/insertMagrittrPipeAddin.R
@@ -0,0 +1,8 @@
+#' Insert \%>\% .
+#'
+#' Call this function as an addin to insert \code{\link[magrittr]{\%>\%}} (Magrittr pipe, \url{https://CRAN.R-project.org/package=magrittr}) at the cursor position.
+#'
+#' @export
+insertMagrittrPipeAddin <- function() {
+  rstudioapi::insertText(" %>%\n\t")
+}

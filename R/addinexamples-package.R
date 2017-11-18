572d363eb2707bbf1e389f54f0fcd874d99c734b
John Mount <jmount@win-vector.com>
2017-11-18 08:05:41 -0800

    fix description

--- R/addinexamples-package.R
+++ R/addinexamples-package.R
@@ -1,6 +1,8 @@
-#' addinexamplesWV (Win-Vector fork of \url{https://github.com/rstudio/addinexamples})
+#' addinexamplesWV
 #'
 #' Supplies insertDotPipeAdin.
+#'
+#' This package (\url{https://github.com/WinVector/addinexamplesWV}) is a fork of \url{https://github.com/rstudio/addinexamples}.
 #'
 #' @name addinexamplesWV
 #' @docType package
